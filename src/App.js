f45a4414580d1408461cf3a3c028e4855ff79dcf
dhiya <ssandhiya481@gmail.com>
2021-05-09 12:34:13 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterContainer } from './container/footer'
+
+function App() {
+  return (
+   <>
+    <FooterContainer />
+   </>
+  );
+}
+
+export default App;
